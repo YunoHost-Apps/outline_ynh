30137822424f0f70cc1dad00138f52585541cc70
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-03-16 14:54:19 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-nodejs_version=18
+nodejs_version="18"
 
 # FIXME : to be replaced in the future by ynh_string_random(hex)
 ynh_hex_32_random() {
